3b5b8d2c32ecc7614005022a56679ba8a0744d36
Alena <i.am.am.629@gmail.com>
2018-07-31 23:28:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = new Object()
+
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function deleteFromObjectByKey(object, key){
+  
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
